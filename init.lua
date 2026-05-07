688700761f2d778eecf3513e40956c399f4aa4fc
Morgan Hardwood <bugs@londonlight.org>
2026-05-07 18:27:18 +0200

    Use system clang-format, more

--- init.lua
+++ init.lua
@@ -771,7 +771,9 @@ require('lazy').setup({
       --
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
+      local ensure_installed = vim.tbl_filter(function(server)
+        return server ~= 'clangd'
+      end, vim.tbl_keys(servers or {}))
       vim.list_extend(ensure_installed, {
         'stylua',
         'cpplint',
